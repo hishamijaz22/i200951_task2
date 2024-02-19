8ca20ffe26db0de06df551c3f74726aaba06c833
hishamijaz22 <159747413+hishamijaz22@users.noreply.github.com>
2024-02-19 09:42:08 +0500

    Update test.py

--- test.py
+++ test.py
@@ -1,13 +1,18 @@
-'import pytest' 
-'from main import StudentsInMLOps' 
-'' 
-'def test_enrollStudents():' 
-'    ml_ops = StudentsInMLOps()' 
-'    ml_ops.enrollStudents(10)' 
-'    assert ml_ops.getTotalStrength() == 10' 
-'' 
-'def test_dropStudents():' 
-'    ml_ops = StudentsInMLOps()' 
-'    ml_ops.enrollStudents(10)' 
-'    ml_ops.dropStudents(5)' 
-'    assert ml_ops.getTotalStrength() == 5' 
+from main import StudentsInMLOps
+
+def test_enroll_and_drop_students():
+    classroom = StudentsInMLOps()
+    classroom.enrollStudents(5)
+    assert classroom.getTotalStrength() == 5
+    classroom.dropStudents(2)
+    assert classroom.getTotalStrength() == 3
+
+def test_total_strength():
+    classroom = StudentsInMLOps()
+    assert classroom.getTotalStrength() == 0
+    classroom.enrollStudents(10)
+    assert classroom.getTotalStrength() == 10
+
+def test_class_name():
+    classroom = StudentsInMLOps()
+    assert classroom.getClassName() == "StudentsInMLOps"
