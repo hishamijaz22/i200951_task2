ddc3402eb6c2f501ea6c8adcd0f954c91c1702f3
hishamijaz22 <159747413+hishamijaz22@users.noreply.github.com>
2024-02-19 09:47:52 +0500

    Update test.py

--- test.py
+++ test.py
@@ -1,19 +1,17 @@
 from main import StudentsInMLOps
 import pytest
 
-def test_enroll_and_drop_students():
-    classroom = StudentsInMLOps()
-    classroom.enrollStudents(5)
-    assert classroom.getTotalStrength() == 5
-    classroom.dropStudents(2)
-    assert classroom.getTotalStrength() == 3
+def test_enrollStudents():
+    students = StudentsInMLOps()
+    students.enrollStudents(5)
+    assert students.getTotalStrength() == 5
 
-def test_total_strength():
-    classroom = StudentsInMLOps()
-    assert classroom.getTotalStrength() == 0
-    classroom.enrollStudents(10)
-    assert classroom.getTotalStrength() == 10
+def test_dropStudents():
+    students = StudentsInMLOps()
+    students.enrollStudents(10)
+    students.dropStudents(3)
+    assert students.getTotalStrength() == 7
 
-def test_class_name():
-    classroom = StudentsInMLOps()
-    assert classroom.getClassName() == "StudentsInMLOps"
+def test_getClassName():
+    students = StudentsInMLOps()
+    assert students.getClassName() == "StudentsInMLOps"
